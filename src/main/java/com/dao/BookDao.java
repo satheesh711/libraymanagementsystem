2b9f6aa1341e2b0cd087b969029c086f55f2d407
Sira Satheesh <satheesh2254@gmail.com>
2025-07-24 11:58:10 +0530

    Book and Isue Dao and model

--- src/main/java/com/dao/BookDao.java
+++ src/main/java/com/dao/BookDao.java
@@ -1,5 +1,17 @@
 package com.dao;
 
+import java.util.List;
+
+import com.domain.Book;
+
 public interface BookDao {
+
+	void addBook(Book book);
+
+	void updateBook(Book book);
+
+	void updateBookAvalability(int id, char avalability);
+
+	List<Book> getAllBooks();
 
 }
--- src/main/java/com/dao/impl/BookDaoImpl.java
+++ src/main/java/com/dao/impl/BookDaoImpl.java
@@ -1,7 +1,34 @@
 package com.dao.impl;
 
+import java.util.List;
+
 import com.dao.BookDao;
+import com.domain.Book;
 
 public class BookDaoImpl implements BookDao {
+
+	@Override
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateBookAvalability(int id, char avalability) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
