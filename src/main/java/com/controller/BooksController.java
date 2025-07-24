188ccbdf35f366c5854dce2f30c5c3f7d9f7b1c5
charan11368 <charancharan11368@gmail.com>
2025-07-24 17:07:58 +0530

    fxml for book

---
+++ src/main/java/com/controller/BooksController.java
@@ -0,0 +1,22 @@
+//package com.controller;
+//
+//import javafx.event.ActionEvent;
+//
+//public class BooksController {
+//	public void Book(ActionEvent e) {
+//
+//	}
+//
+//	public void Members(ActionEvent e) {
+//
+//	}
+//
+//	public void Issue(ActionEvent e) {
+//
+//	}
+//
+//	public void Book(ActionEvent e) {
+//
+//	}
+//
+//}
