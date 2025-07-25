00e50b0f510367307e2645ead6df194dea514ed7
Sira Satheesh <satheesh2254@gmail.com>
2025-07-25 16:30:44 +0530

    just sync

--- src/main/java/com/controller/Books/BookUpdateController.java
+++ src/main/java/com/controller/Books/BookUpdateController.java
@@ -1,10 +1,33 @@
 package com.controller.Books;
 
 import com.libraryManagementSystem.App;
+import com.services.BookServices;
+import com.services.impl.BookServicesImpl;
+import com.utilities.BookAvailability;
+import com.utilities.BookCategory;
+import com.utilities.BookStatus;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class BookUpdateController {
+
+	BookServices bookService = new BookServicesImpl();
+
+	@FXML
+	private ComboBox<BookCategory> category;
+	@FXML
+	private ComboBox<BookStatus> status;
+	@FXML
+	private ComboBox<BookAvailability> availability;
+	@FXML
+	private TextField title;
+	@FXML
+	private TextField author;
+	@FXML
+	private Label error;
 
 	@FXML
 	private void switchToBookOptions() throws Exception {
@@ -15,4 +38,5 @@ public class BookUpdateController {
 	private void switchToPrimary() throws Exception {
 		App.setRoot("primary");
 	}
+
 }
