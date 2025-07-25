e344ee3b5eb04488d59aa6ea5ae7273320cc9c50
Sira Satheesh <satheesh2254@gmail.com>
2025-07-25 17:25:43 +0530

    update on book

--- src/main/java/com/controller/Books/BookUpdateController.java
+++ src/main/java/com/controller/Books/BookUpdateController.java
@@ -1,5 +1,10 @@
 package com.controller.Books;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import com.domain.Book;
 import com.libraryManagementSystem.App;
 import com.services.BookServices;
 import com.services.impl.BookServicesImpl;
@@ -8,14 +13,17 @@ import com.utilities.BookCategory;
 import com.utilities.BookStatus;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class BookUpdateController {
+public class BookUpdateController implements Initializable {
 
 	BookServices bookService = new BookServicesImpl();
 
+	@FXML
+	private ComboBox<Book> books;
 	@FXML
 	private ComboBox<BookCategory> category;
 	@FXML
@@ -37,6 +45,25 @@ public class BookUpdateController {
 	@FXML
 	private void switchToPrimary() throws Exception {
 		App.setRoot("primary");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		List<Book> bookTitles = bookService.getBooks();
+		books.getItems().addAll(bookTitles);
+		books.setOnAction(event -> {
+			if (!books.getSelectionModel().isEmpty()) {
+				Book book = books.getSelectionModel().getSelectedItem();
+				category.setDisable(false);
+				category.getItems().addAll(BookCategory.values());
+				category.getSelectionModel().select(book.getCategory());
+
+			}
+		});
+
+//		status.getItems().addAll(BookStatus.values());
+//		availability.getItems().addAll(BookAvailability.values());
+
 	}
 
 }
