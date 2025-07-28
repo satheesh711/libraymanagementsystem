ed635aa351ee360c7d17bc67e0ec890548658f08
Sira Satheesh <satheesh2254@gmail.com>
2025-07-28 20:01:10 +0530

    error

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module library.libraryManagementSystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens library.libraryManagementSystem to javafx.fxml;
-    exports library.libraryManagementSystem;
-}
-*/
